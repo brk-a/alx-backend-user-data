f70fa84576452304d7905db778ca2ee36563276b
brk-a <fnjakai@outlook.com>
2022-05-25 00:09:56 -0700

    0x03-user_authentication_service

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+App.py: entry point
+'''
+
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))   
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''
+Takes in a password string arguments and returns bytes.
+The returned bytes is a salted hash of the input password
+'''
+
+import bcrypt
+from db import DB
+from user import User
+salt = bcrypt.gensalt()
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password"""
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user to the db"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
+        else:
+            raise ValueError('User {email} already exists')
